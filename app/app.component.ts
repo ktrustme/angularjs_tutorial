49d15cd07d3acd860e9f2d86f236f2c9d54cf3f7
Kuo <kuoxin@amazon.com>
2016-09-02 17:05:06 -0700

    finished the tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,19 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-    id: number;
-    name: string;
-}
+import { Hero } from './hero';
+import { HeroService } from './hero.service'
 
 @Component({
     selector: 'my-app',
-    template: `<h1>{{title}}</h1>
-                <h2>{{hero.name}} details!</h2>
-                <div><label>id: </label>{{hero.id}}</div>
-                <div>
-                    <label>name: </label>
-                    <input [(ngModel)]="hero.name" placeholder="name">
-                    {{hero.name}}
-                </div>
-                `
+    template: ` <h1>{{title}}</h1>
+                <nav>
+                    <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+                    <a routerLink="/heroes" routerLinkActive="active"  >Heroes</a>
+                </nav>
+                <router-outlet></router-outlet>
+    `,
+    styleUrls: ['app/app.component.css'] 
 })
-
 export class AppComponent { 
     title = 'Tour of Heros';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };                    
 }
+
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,37 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { HeroDetailComponent } from './hero-detail.component';
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { routing } from './app.routing';
+
+
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [ 
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService), 
+    routing
   ],
-  declarations: [ AppComponent ],
+  declarations: [ 
+    AppComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroDetailComponent
+  ],
+  providers: [
+    HeroService
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
